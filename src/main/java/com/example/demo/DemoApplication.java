87354023beba07f1ec9f7aa9db22ed4d6aac508c
pantyash74@gmail.com <pantyash74@gmail.com>
2021-08-30 23:33:43 +0530

    Basic API implementation

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.Month;
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
